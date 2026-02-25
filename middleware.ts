a1880b97eff1c368a1c4f2bc11d8ab8820b1b7c8
Ilyes <ilyeesx@gmail.com>
2026-02-25 02:08:09 +0100

    add middleware

---
+++ middleware.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next()
+  
+  // Autorise GTM + GA4
+  const csp = `
+    default-src 'self';
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' 
+      https://www.googletagmanager.com 
+      https://www.google-analytics.com 
+      https://www.googleadservices.com 
+      https://googleads.g.doubleclick.net;
+    script-src-elem 'self' 'unsafe-inline'
+      https://www.googletagmanager.com 
+      https://www.google-analytics.com 
+      https://www.googleadservices.com;
+    img-src 'self' data: https: blob:;
+    connect-src 'self' https://www.google-analytics.com https://*.googleapis.com;
+    frame-src 'self' https://www.googletagmanager.com;
+  `
+  
+  response.headers.set('Content-Security-Policy', csp)
+  
+  return response
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
